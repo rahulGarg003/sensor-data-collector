82be3f2e59678d68632bbb3d5bfbd148cc638720
rahulGarg003 <rahul.garg9460@gmail.com>
2023-08-22 18:34:21 +0530

    Updated code

--- sensor-simulator/app.py
+++ sensor-simulator/app.py
@@ -1,9 +1,59 @@
 import os
 import paho.mqtt.client as mqtt
+from datetime import datetime
+import time
+from logger import logging
+from random import randint
 
-sensor_id = os.environ['SENSOR-ID']
+sensor_id = os.environ.get('SENSOR-ID', 1)
+mqtt_broker_host = os.environ.get("MQQT-HOSTNAME", 'localhost')
+mqtt_broker_port = int(os.environ.get("MQQT-PORT", 1883))
+# mqtt_broker_username = 'mqtt-admin'
+# mqtt_broker_password = 'mqtt-admin'
+
+
+def create_mqqt_client() -> mqtt.Client:
+    logging.info(f'trying to create mqqt client with HostName -> {mqtt_broker_host} and Port -> {mqtt_broker_port}')
+    client = mqtt.Client(client_id=sensor_id)
+    # client.username_pw_set(username=mqtt_broker_username, password=mqtt_broker_password)
+    # client.on_connect = on_connect
+    # client.on_message = on_message
+    client.connect(host=mqtt_broker_host, port=mqtt_broker_port)
+    logging.info('Connection to mqtt broker Successfully established')
+
+    return client
+
+
+# def on_connect(client, userdata, flags, rc):
+#     logging.error('-------------Connect Call-------------------')
+#     print(client)
+#     print(userdata)
+#     print(flags)
+#     print(rc)
+
+# def on_message(client, userdata, msg):
+#     logging.error("--------------Message Call------------------")
+#     print(client)
+#     print(userdata)
+#     print(msg)
+#     print(f"{msg.topic} {str(msg.payload)}")
 
 
 
-print('Hi this is running')
-print(sensor_id)
+client = create_mqqt_client()
+client.loop_start()
+while True:
+    message_payload = {
+        "sensor_id": sensor_id, 
+        "value": randint(5, 50), 
+        "timestamp": datetime.now().isoformat()
+    }
+    logging.info(str(message_payload))
+    msginfo = client.publish(
+        topic=os.environ.get('MQQT-TOPIC-TEMPRATURE','sensor/temprature'), 
+        payload=str(message_payload)
+    )
+    time.sleep(5)
+
+# client.loop_forever()
+
---
+++ sensor-simulator/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+import sys
+
+# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{os.environ.get('SENSOR-ID', 1)}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    format = "%(asctime)s [%(levelname)-8s] %(lineno)-5d %(name)-10s - %(message)s",
+    level = logging.INFO,
+
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(LOG_FILE_PATH)
+    ]
+)
